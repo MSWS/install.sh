576a3e6a751b424ef25807eeb105509bfb493a4c
MSWS <imodmaker@gmail.com>
2024-02-12 02:56:19 -0800

    Install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git config --global user.name "MSWS"
+git config --global user.email "imodmaker@gmail.com"
+
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+git config --global user.signingKey ~/.ssh/yubikey
+
+git config --global init.defaultBranch main
+git config --global push.autosetupremote=true
+
+git config --global alias.ca 'commit -am'
