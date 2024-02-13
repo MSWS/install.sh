d686c81e94c12ec06f15e44ead89cb0f5d7cc9d0
MSWS <imodmaker@gmail.com>
2024-02-13 09:52:23 -0800

    Add ssh configurations

--- install.sh
+++ install.sh
@@ -11,3 +11,11 @@ git config --global init.defaultBranch main
 git config --global push.autosetupremote=true
 
 git config --global alias.ca 'commit -am'
+
+cp ./ssh_config ~/.ssh/config
+
+echo "eval $(ssh-agent -s) &>/dev/null" >> ~/.bashrc
+eval $(ssh-agent -s)
+ssh-keygen -K
+mv id_ed25519_sk_rk ~/.ssh/yubikey
+mv id_ed25519_sk_rk.pub ~/.ssh/yubikey.pub
